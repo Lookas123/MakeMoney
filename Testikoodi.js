e907416d98ac09011145983d1f2e5ccdb004a9cc
Lookas123 <42605685+Lookas123@users.noreply.github.com>
2020-07-21 17:13:23 +0300

    parempi koodi+respec

--- Testikoodi.js
+++ Testikoodi.js
@@ -1,28 +1,29 @@
-var x = 1;
-var moneymakers = 0
-var cost = 30
-var mult = 1
-var upgradecost = 100;
-var clickstrength = 1;
-var money;
-var mupgradecost = 200;
-var cash = 0;
-var availablecash = 0;
-var cups = [];
-var upgrademult=1;
-var mupgrademult = 1;
-var totalcash=0;
-var costscalepoint = 100000;
-var cashformula = 1;
-var cup6cost = 1;
-var moneymult = 1;
-
+let x = 1;
+let moneymakers = 0
+let cost = 30
+let clickupgrades = 0;
+let mupgrades = 0;
+let upgradecost = 100;
+let money;
+let mupgradecost = 200;
+let cash = 0;
+let availablecash = 0;
+let cups = [];
+let upgrademult=1;
+let mupgrademult = 1;
+let totalcash=0;
+const costscalepoint = 100000;
+let cashformula = 1;
+let cup6cost = 1;
+let moneymult = 1;
+const cupprices = [5,10,10,1]
 
 // click
-function double(){
-    x += clickstrength * (cash +1) * moneymult;
-    money = document.getElementById("money");
-    money.innerHTML = "Money: " + format(x);
+function double(auto = false){
+    if(!auto || cups.includes(5)) {
+        x += (2*(cups.includes(2)?1.25:1)) ** clickupgrades * (cash +1) * moneymult;
+        document.getElementById("money").innerHTML = "Money: " + format(x);
+    }
 }
 
 
@@ -34,32 +35,32 @@ function update(){
 	availablecash += totalcash;    
     }
     if (moneymakers>0 || totalcash>0) {
-    document.getElementById("moneymakers").innerHTML = "You have " + moneymakers + " moneymakers, making " + format(moneymakers*mult*(cash+1)*moneymult) + " money per second" 
+    document.getElementById("moneymakers").innerHTML = "You have " + moneymakers + " moneymakers, making " + format(moneymakers*((1.5*(cups.includes(1)?1.25:1))**mupgrades)*(cash+1)*moneymult) + " money per second" 
     }
     if (availablecash<0) {
         availablecash=0
     }
     if (totalcash >0) {
-    document.getElementById("cash").innerHTML = "You have " + cash + " cash, making you make " + cash + " times more money"
+    document.getElementById("cash").innerHTML = "You have " + format(cash) + " cash, making you make " + format(cash) + " times more money"
     }
     if (availablecash > 0 || totalcash>0) {
         show("prestige") 
-    document.getElementById("prestige").innerHTML = "Sell everything and get " + availablecash + " cash!"
+    document.getElementById("prestige").innerHTML = "Sell everything and get " + format(availablecash) + " cash!"
     }else {
         hide("prestige")
     }
-    document.getElementById("click").innerHTML = "Click here for " + format(clickstrength *(cash+1)*moneymult) + " money"
+    document.getElementById("click").innerHTML = "Click here for " + format((2*(cups.includes(2)?1.25:1)) ** clickupgrades * (cash +1) * moneymult) + " money"
     if (x<20&&moneymakers==0&&totalcash==0) {
         hide("buy")
     }else{
         show("buy")
     }
-    if (x<75&&totalcash==0&&clickstrength==1) {
+    if (x<75&&totalcash==0&&clickupgrades==0) {
         hide("upgrade")
     }else{
         show("upgrade")
     }
-    if (x<150 && totalcash==0&& mult==1 ) {
+    if (x<150 && totalcash==0&& mupgrades==0) {
         hide("mupgrade")
     }else{
         show("mupgrade")
@@ -70,14 +71,16 @@ function update(){
         hide("cup3")
         hide("cup4")
         hide("cup5")
-	hide("cup6")
+        hide("cup6")
+        hide("respec")
     }else{
         show("cup1")
         show("cup2")
         show("cup3")
         show("cup4")
         show("cup5")
-	show("cup6")
+        show("cup6")
+        show("respec")
     }
     if (x>=upgradecost){
         document.getElementById("upgrade").classList.add("buyable")
@@ -142,17 +145,17 @@ function update(){
     
 }
 function gainmoney() {
-    x += Math.round(moneymakers*mult*(cash+1)*moneymult)
+    x += Math.round(moneymakers*((1.5*(cups.includes(1)?1.25:1))**mupgrades)*(cash+1)*moneymult)
 }
 
 function upgrade(){
     if (x >= upgradecost) {
      x-= upgradecost
      upgradecost *=3 
-     if (upgradecost > costscalepoint) {
+     if (upgradecost > getcostscalepoint()) {
          upgradecost *= Math.round(Math.log10(upgradecost)/3)
      }
-     clickstrength *=2 * upgrademult;
+     clickupgrades++;
      document.getElementById("upgrade").innerHTML = "Upgrade your clicks for " + format(upgradecost) + " money";
      document.getElementById("money").innerHTML = "Money: " + format(x); 
     }
@@ -162,11 +165,11 @@ function upgrade(){
 function mupgrade(){
     if (x >= mupgradecost) {
      x -= mupgradecost
-     mupgradecost *=2
-     if (mupgradecost > costscalepoint) {
+     mupgradecost *= 2
+     if (mupgradecost > getcostscalepoint()) {
          mupgradecost *= Math.round(Math.log10(mupgradecost)/3)
      } 
-     mult *= 1.5 * mupgrademult
+     mupgrades++;
      document.getElementById("mupgrade").innerHTML = "Upgrade your moneymakers for " + format(mupgradecost) + " money";
      document.getElementById("money").innerHTML = "Money: " + format(x); 
     }
@@ -189,8 +192,9 @@ function prestige() {
      moneymakers = 0
      cost = 30
      mult = 1
-     upgradecost = 100;
-     clickstrength = 1;
+     mupgrades = 0;
+     clickupgrades = 0;
+     upgradecost=100;
      mupgradecost = 200;
      totalcash += availablecash;
      cash += availablecash;
@@ -198,14 +202,13 @@ function prestige() {
      document.getElementById("mupgrade").innerHTML = "Upgrade your moneymakers for " + mupgradecost + " money";
      document.getElementById("upgrade").innerHTML = "Upgrade your clicks for " + upgradecost + " money";
      document.getElementById("buy").innerHTML = "Buy a moneymaker for " + cost + " money"
-
      hide("prestige")
 }
 
 
 //convert a number to scientific
 function format(a) {
-    if (a>1000) {
+    if (a>=1000) {
         return Math.round(a/Math.pow(10, Math.floor(Math.log10(a)))*100)/100+"e"+Math.floor(Math.log10(a))
     }else{
         return Math.round(a)
@@ -213,46 +216,18 @@ function format(a) {
 
 }
 
-//prestige upgrades
-//1
-function buy1() {
-    if (cash>=5 && !cups.includes(1)){
-        cash -= 5;
-        document.getElementById("cup1").classList.add("bought")
-        cups.push(1)
-        mupgrademult=1.25
+function getcostscalepoint(){
+    return costscalepoint *(cups.includes(3)?1000:1)
+}
+
+function buynormal(cup){
+    if(cash>=cupprices[cup-1] && !cups.includes(cup)){
+        cash-=cupprices[cup-1];
+        cups.push(cup);
+        document.getElementById("cup"+cup).classList.add("bought")
     }
 }
 
-//2
-function buy2() {
-    if (cash>=10 && !cups.includes(2)){
-        cash -= 10;
-        document.getElementById("cup2").classList.add("bought")
-        cups.push(2)
-        upgrademult=1.25
-    }
-}
-
-//3
-function buy3() {
-    if (cash>=10 && !cups.includes(3)){
-        cash -= 10;
-        document.getElementById("cup3").classList.add("bought")
-        cups.push(3)
-        costscalepoint = 100000000;
-    }
-}
-
-//4
-function buy4() {
-    if (cash>=1 && !cups.includes(4)){
-        cash -= 1;
-        document.getElementById("cup4").classList.add("bought")
-        cups.push(4)
-        setInterval(double, 200);
-    }
-}
 
 //5
 function buy5() {
@@ -269,10 +244,29 @@ function buy5() {
 function buy6() {
     if (cash>=cup6cost){
         cash -= cup6cost;
-	moneymult *= 5;
-	cup6cost *= 10;   
-        document.getElementById("cup6").innerHTML =  "Quintuple the amount of money you earn for " + cup6cost + " cash"     
+	    moneymult *= 5;
+	    cup6cost *= 10;   
+        document.getElementById("cup6").innerHTML =  "Quintuple the amount of money you earn for " + format(cup6cost) + " cash"     
     }
+}
+
+function respec(){
+    for(i=0; i<cupprices.length; i++){
+        if(cups.includes(i+1)) cash+=cupprices[i];
+        document.getElementById("cup"+(i+1)).classList.remove("bought")
+    }
+    if(cups.includes(5)){
+        cash+=30;
+        cashformula=1;
+        document.getElementById("cup5").classList.remove("bought")
+    }
+    while(cup6cost>1){
+        cash+=cup6cost/10;
+        cup6cost/=10;
+    }
+    moneymult=1;
+    cups=[];
+    document.getElementById("cup6").innerHTML =  "Quintuple the amount of money you earn for " + format(cup6cost) + " cash"
 }
 
 function hide(x) {
@@ -285,13 +279,4 @@ function show(x) {
 
 setInterval(update, 50)
 setInterval(gainmoney, 1000)
-
-
-
-
-
-
-
-
-
-
+setInterval(double, 200, true);
